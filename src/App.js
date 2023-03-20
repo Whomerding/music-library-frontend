13f579f83f5cec54acae90757e40d6dc7d11d7a1
Whomerding <wendy.homerding@gmail.com>
2023-03-20 13:59:34 -0400

    working on post new song component

--- src/App.js
+++ src/App.js
@@ -2,8 +2,20 @@ import React, { useState, useEffect } from "react";
 import NavigationBar from "./Components/NavigationBar/NavigationBar";
 import TableDisplay from "./Components/TableDisplay/TableDisplay";
 import axios from "axios";
+import AddMusic from "./Components/AddMusic/AddMusic";
 function App() {
-  const [songs, setSongs] = useState ()
+  const [song, setSong] = useState ()
+
+  function addNewSong(song){
+    let tempSong = [song];
+    setSong(tempSong);
+    axios.post('http://127.0.0.1:8000/api/music/', tempSong)
+        .then(response => this.setState({ tempSong: [ "title:": response.data.title ]}))
+        .catch(error => {
+            this.setState({ errorMessage: error.message });
+            console.error('There was an error!', error);
+        });
+  }
 
   useEffect(() => {
     tableResponses();
@@ -18,7 +30,8 @@ function App() {
   return (
     <div>
       <NavigationBar />
-      <TableDisplay parentSongs = {songs} />
+      <TableDisplay parentSongs = {song} />
+      <AddMusic addNewSongProperty = {addNewSong}/>
     </div>
   );
 }
